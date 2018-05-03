50d3ead7f36a4f90d4ddeefdc01f220c7997dc88
Zuohao She <zuohaoshe2013@u.northwestern.edu>
2018-05-03 11:25:49 -0500

    Course reconciliation reformatted

--- entity_matchers/CourseResolver.py
+++ entity_matchers/CourseResolver.py
@@ -19,19 +19,19 @@ def levenshteinDistance(s1, s2):
         distances = distances_
     return distances[-1]
 
+class crc:
+	name_resolver_path = './results/name_resolver.jl'
 
 class CourseResolver:
 	def __init__(self):
 		self.cc = CourseCatalog()
 		self.ugms = UndergraduateManualScraper()
-		self.ugms.conv_ee()
-		self.ugms.conv_ce()	
-		self.ugms.conv_cs()
-		self.fn = NameResolver(SimpleLogger(), 'name_resolver.jl')
+		self.ugms.convert_all_disciplines()
+		self.fn = NameResolver(SimpleLogger(), crc.name_resolver_path)
+		self.out = []
+		self.no_instr = []
+		self.no_match = []
 	def resolve(self):
-		out = []
-		no_instr = []
-		no_match = []
 		for key in self.cc.courses_map:
 			cc_course = self.cc.courses_map[key]
 			for c1 in cc_course:
@@ -48,7 +48,7 @@ class CourseResolver:
 								discipline_set.add(ugc.discipline)
 				# resolve with faculty name
 				if len(discipline_set) > 0:
-					out.append(c1.primary_course_id + ' ' + c1.course_name + ' ' + str(list(discipline_set)))
+					self.out.append(c1.primary_course_id + ' ' + c1.course_name + ' ' + str(list(discipline_set)))
 					c1.disciplines.extend(list(discipline_set))
 				else:
 					# print c1
@@ -69,16 +69,16 @@ class CourseResolver:
 								for i in self.fn.fullNameDict[fac][0].divisions:
 									discipline_set.add(i)
 						if len(discipline_set) == 0:
-							no_match.append(key+ ' ' + c1.course_name + ' ' + str(list(instructorSet)))
+							self.no_match.append(key+ ' ' + c1.course_name + ' ' + str(list(instructorSet)))
 							# print '--- Cannot match instructor: ', c1, '; candidates: ', list(instructorSet)
 						else:
 							c1.disciplines.extend(list(discipline_set))
-							out.append(key+ ' ' + c1.course_name + ' ' + str(list(discipline_set)))
+							self.out.append(key+ ' ' + c1.course_name + ' ' + str(list(discipline_set)))
 					else:
-						no_instr.append(key+ ' ' + c1.course_name)
-		out.sort()
-		no_instr.sort()
-		no_match.sort()
+						self.no_instr.append(key+ ' ' + c1.course_name)
+		self.out.sort()
+		self.no_instr.sort()
+		self.no_match.sort()
 		# print 
 		# print "Matches"
 		# print
@@ -94,45 +94,51 @@ class CourseResolver:
 		# print
 		# for i in no_instr:
 		# 	print i
+	def ontologize(self):
+		print '(in-microtheory CoursesMt)'
+		print 
+
+		timeOfCourseFn = '(timeOfCourseFn (courseSpringOffering %s) (courseTime "%s" "%s"))'
+
+		for i in self.cc.courses_map:
+			for j in self.cc.courses_map[i]:
+				print j
+				n = re.sub('[ &]+' ,'', j.course_name)
+				print '(isa %s EECSCourse)' % (n)
+				print '(primaryCourseID %s "%s")' % (n, j.primary_course_id)
+				print '(courseName %s "%s")' % (n, j.course_name)
+				for dis in j.disciplines:
+					dis = re.sub('[ ]+' ,'', dis)
+					print '(courseDiscipline %s %s)' % (n, dis)
+				if len(j.offering_detail[0]) > 0:
+					print '(courseFallOffering %s)' % (n)
+					for fac in j.offering_detail[0][0]:
+						fac = re.sub('[ .&]+' ,'', str(fac))
+						print '(instructorOfCourseFn (CourseFallOffering %s) %s)' % (n, fac)
+					for time in j.offering_detail[0][1]:
+						print '(timeOfCourseFn (courseFallOffering %s) (courseTime "%s" "%s")' % (n, time[0], time[1])
+				if len(j.offering_detail[1]) > 0:
+					print '(courseWinterOffering %s)' % (n)
+					for fac in j.offering_detail[1][0]:
+						fac = re.sub('[ .&]+' ,'', str(fac))
+						print '(instructorOfCourseFn (courseWinterOffering %s) %s)' % (n, fac)
+					for time in j.offering_detail[1][1]:
+						print '(timeOfCourseFn (courseWinterOffering %s) (courseTime "%s" "%s")' % (n, time[0], time[1])
+				if len(j.offering_detail[2]) > 0:
+					print '(courseSpringOffering %s)' % (n)
+					for fac in j.offering_detail[2][0]:
+						fac = re.sub('[ .&]+' ,'', str(fac))
+						print '(instructorOfCourseFn (courseSpringOffering %s) %s)' % (n, fac)
+					for time in j.offering_detail[2][1]:
+						print timeOfCourseFn % (n, time[0], time[1])
+				print
 
 
 def __main__():
 	cr = CourseResolver()
 	cr.resolve()
-	print '(in-microtheory CoursesMt)'
-	print 
-	for i in cr.cc.courses_map:
-		for j in cr.cc.courses_map[i]:
-			print j
-			n = re.sub('[ &]+' ,'', j.course_name)
-			print '(isa %s EECSCourse)' % (n)
-			print '(primaryCourseID %s "%s")' % (n, j.primary_course_id)
-			print '(courseName %s "%s")' % (n, j.course_name)
-			for dis in j.disciplines:
-				dis = re.sub('[ ]+' ,'', dis)
-				print '(courseDiscipline %s %s)' % (n, dis)
-			if len(j.offering_detail[0]) > 0:
-				print '(courseFallOffering %s)' % (n)
-				for fac in j.offering_detail[0][0]:
-					fac = re.sub('[ .&]+' ,'', str(fac))
-					print '(instructorOfCourseFn (CourseFallOffering %s) %s)' % (n, fac)
-				for time in j.offering_detail[0][1]:
-					print '(timeOfCourseFn (courseFallOffering %s) (courseTime "%s" "%s")' % (n, time[0], time[1])
-			if len(j.offering_detail[1]) > 0:
-				print '(courseWinterOffering %s)' % (n)
-				for fac in j.offering_detail[1][0]:
-					fac = re.sub('[ .&]+' ,'', str(fac))
-					print '(instructorOfCourseFn (courseWinterOffering %s) %s)' % (n, fac)
-				for time in j.offering_detail[1][1]:
-					print '(timeOfCourseFn (courseWinterOffering %s) (courseTime "%s" "%s")' % (n, time[0], time[1])
-			if len(j.offering_detail[2]) > 0:
-				print '(courseSpringOffering %s)' % (n)
-				for fac in j.offering_detail[2][0]:
-					fac = re.sub('[ .&]+' ,'', str(fac))
-					print '(instructorOfCourseFn (courseSpringOffering %s) %s)' % (n, fac)
-				for time in j.offering_detail[2][1]:
-					print '(timeOfCourseFn (courseSpringOffering %s) (courseTime "%s" "%s"))' % (n, time[0], time[1])
-			print
+	cr.ontologize()
+	
 
 
 
--- entity_matchers/ManualScrapers.py
+++ entity_matchers/ManualScrapers.py
@@ -13,9 +13,28 @@ from unidecode import unidecode
 
 import re
 
+def convert_pdf_to_txt(path):
+	rsrcmgr = PDFResourceManager()
+	retstr = StringIO()
+	codec = 'utf-8'
+	laparams = LAParams()
+	device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+	fp = file(path, 'rb')
+	interpreter = PDFPageInterpreter(rsrcmgr, device)
+	password = ""
+	maxpages = 0
+	caching = True
+	pagenos=set()
+	for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+		interpreter.process_page(page)
+	text = retstr.getvalue()
+
+	fp.close()
+	device.close()
+	retstr.close()
+	return text
 
 class ManualCourseInfo:
-
 	def __init__(self, dept, course_id, discipline, course_name):
 		self.dept = dept
 		self.primary_course_id = course_id[0]
@@ -29,86 +48,51 @@ class ManualCourseInfo:
 	def __repr__(self):
 		return 'EECS ' + self.primary_course_id + ' - ' + self.course_name + ' - ' + self.discipline
 
+class umsc:
+	manual_loc = './inputs/eecs-undergraduate-manual.pdf'
+	manual_url = 'https://www.mccormick.northwestern.edu/eecs/documents/undergraduate/eecs-undergraduate-manual.pdf'
+	manual_txt = './inputs/ug_manual_text.txt'
+	manual_pdf = './inputs/eecs-undergraduate-manual.pdf'
+	manual_title ='EECS Undergraduate Study Manual'
+	cs_start = 'Students, who have never programmed before, in any language'
+	cs_end = '4.5 Project Requirement'
+	ce_start = 'Our curriculum is continuously revised based on feedback from our constituents, e'
+	ce_end = 'Among the 16 departmental courses, the P/N option may only'	
+	ee_start = 'Systems including Digital Signal Processing'
+	ee_end = 'Among the 16 departmental courses, the P/N option may only'
 
 class UndergraduateManualScraper:
 	CoursePattern = re.compile('EECS[-, :/0-9]+([(].+[0-9]+[)])?[- ]*')
-	ParenthesizedPattern = re.compile('[(].+[)]')
+	ParenthesizedPattern = re.compile('[(].+[)]')	
+
 	def __init__(self):
-		if not os.path.isfile('./eecs-undergraduate-manual.pdf'):
-			urllib.urlretrieve('https://www.mccormick.northwestern.edu/eecs/documents/undergraduate/eecs-undergraduate-manual.pdf', './eecs-undergraduate-manual.pdf')
-		elif not os.path.isfile('./ug_manual_text.txt'):
-			# self.text = re.sub(r'[^\x00-\x7F]+',' ', self.convert_pdf_to_txt('./eecs-undergraduate-manual.pdf'))
-			self.text = self.convert_pdf_to_txt('./eecs-undergraduate-manual.pdf')
-			manual = open('./ug_manual_text.txt', 'w')
+		if not os.path.isfile(umsc.manual_loc):
+			urllib.urlretrieve(umsc.manual_url, umsc.manual_loc)
+		elif not os.path.isfile(umsc.manual_txt):
+			self.text = self.convert_pdf_to_txt(umsc.manual_pdf)
+			manual = open(umsc.manual_txt, 'w')
 			manual.write(self.text)
 			manual.close()
 		else:
-			manual = open('./ug_manual_text.txt', 'r')
+			manual = open(umsc.manual_txt, 'r')
 			self.text = manual.read()
 			manual.close()
 		self.mapping = {}
-
-	def convert_pdf_to_txt(self, path):
-	    rsrcmgr = PDFResourceManager()
-	    retstr = StringIO()
-	    codec = 'utf-8'
-	    laparams = LAParams()
-	    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-	    fp = file(path, 'rb')
-	    interpreter = PDFPageInterpreter(rsrcmgr, device)
-	    password = ""
-	    maxpages = 0
-	    caching = True
-	    pagenos=set()
-	    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
-	        interpreter.process_page(page)
-	    text = retstr.getvalue()
-
-	    fp.close()
-	    device.close()
-	    retstr.close()
-	    return text
-	def conv_cs(self):
+	def convert_all_disciplines(self):
+		self.conv_discipline(umsc.cs_start, umsc.cs_end, 'Computer Science');
+		self.conv_discipline(umsc.ce_start, umsc.ce_end, 'Computer Engineering');
+		self.conv_discipline(umsc.ee_start, umsc.ee_end, 'Electrical Engineering');
+	def conv_discipline(self, start, end, division): 
 		l = self.text.splitlines()
 		lc = len(l)
 		starts = False
 		for i in range(0, lc):
 			li = l[i]
 			if not starts:
-				if li.startswith('Students, who have never programmed before, in any language'):
+				if li.startswith(start):
 					starts = True
 			else:
-				if li.startswith('4.5 Project Requirement'):
-					starts = False
-			if starts:
-				found = None
-				if li.startswith('EECS', 2):
-					found = li[2:]
-				elif li.startswith('EECS', 3):
-					found = li[3:]
-				elif li.startswith('EECS', 4):
-					found = li[4:]
-				elif li.startswith('EECS', 5):
-					found = li[5:]
-				elif li.startswith('EECS', 6):
-					found = li[6:]
-				if found is not None and not found.startswith('EECS Undergraduate Study Manual'):
-				    found = re.sub(' +',' ',found)
-				    c = self.encodeCourse( found, 'Computer Science')
-				    if c.primary_course_id not in self.mapping:
-				    	self.mapping[c.primary_course_id] = []
-				    self.mapping[c.primary_course_id].append(c)
-	def conv_ce(self): 
-		l = self.text.splitlines()
-		lc = len(l)
-		starts = False
-		for i in range(0, lc):
-			li = l[i]
-			if not starts:
-				if li.startswith('Our curriculum is continuously revised based on feedback from our constituents, e'):
-					starts = True
-			else:
-				if li.startswith('Among the 16 departmental courses, the P/N option may only'):
+				if li.startswith(end):
 					starts = False
 			if starts:
 				found = None
@@ -124,45 +108,12 @@ class UndergraduateManualScraper:
 					found = li[5:]
 				elif li.startswith('EECS', 6):
 					found = li[6:]
-				if found is not None and not found.startswith('EECS Undergraduate Study Manual'):
+				if found is not None and not found.startswith(umsc.manual_title):
 					found = re.sub(' +',' ',found)
-					c = self.encodeCourse( found, 'Computer Engineering')
+					c = self.encodeCourse( found, division)
 					if c.primary_course_id not in self.mapping:
 						self.mapping[c.primary_course_id] = []
 					self.mapping[c.primary_course_id].append(c)
-	def conv_ee(self): 
-		l = self.text.splitlines()
-		lc = len(l)
-		starts = False
-		for i in range(0, lc):
-			li = l[i]
-			if not starts:
-				if li.startswith('Systems including Digital Signal Processing'):
-					starts = True
-			else:
-				if li.startswith('Among the 16 departmental courses, the P/N option may only'):
-					starts = False
-			if starts:
-				found = None
-				if li.startswith('EECS') and len(li) > 9:
-					found = li
-				elif li.startswith('EECS', 2):
-					found = li[2:]
-				elif li.startswith('EECS', 3):
-					found = li[3:]
-				elif li.startswith('EECS', 4):
-					found = li[4:]
-				elif li.startswith('EECS', 5):
-					found = li[5:]
-				elif li.startswith('EECS', 6):
-					found = li[6:]
-				if found is not None and not found.startswith('EECS Undergraduate Study Manual'):
-					found = re.sub(' +',' ',found)
-					c = self.encodeCourse( found, 'Electrical Engineering')
-					if c.primary_course_id not in self.mapping:
-						self.mapping[c.primary_course_id] = []
-					self.mapping[c.primary_course_id].append(c)
-
 	def encodeCourse(self, string, discipline): 
 		# string = re.sub('\xe2', '', string)
 		for i in re.finditer(UndergraduateManualScraper.CoursePattern, string):
@@ -216,13 +167,11 @@ class UndergraduateManualScraper:
 
 					
 
-
-
 def __main__():
 	l = UndergraduateManualScraper()
-	l.conv_ee()
-	l.conv_ce()	
-	l.conv_cs()
+	l.conv_discipline(umsc.cs_start, umsc.cs_end, 'Computer Science');
+	l.conv_discipline(umsc.ce_start, umsc.ce_end, 'Computer Engineering');
+	l.conv_discipline(umsc.ee_start, umsc.ee_end, 'Electrical Engineering');
 	for i in l.mapping:
 		print i, l.mapping[i]
 
